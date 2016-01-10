ed2319417f618379e7accf3c480b9f56a3b96863
hyww13 <willy2202@gmail.com>
2016-01-10 16:30:33 +0800

    hw4: util printTable()

--- hw4/src/PlayerB02902009.java
+++ hw4/src/PlayerB02902009.java
@@ -1,10 +1,18 @@
 import foop.*;
 class PlayerB02902009 extends Player{
+	private static void printTable(java.util.ArrayList<Hand> table){
+		for(int i = 0 ; i < table.size() ; i++){
+			for(int j = 0 ; j < table.get(i).getCards().size() ; j++){
+				System.out.println("Hand "+i+"\tCard "+j+" "+table.get(i).getCards().get(j).getSuit()+" "+table.get(i).getCards().get(j).getValue());
+			}
+		}
+	}
 	public PlayerB02902009(int chips){
 		super(chips);
 	}
 	@Override
 	public boolean buy_insurance(Card my_open, Card dealer_open, java.util.ArrayList<Hand> current_table){
+		printTable(current_table);
 		return false;
 	}
 	@Override
@@ -25,7 +33,7 @@ class PlayerB02902009 extends Player{
 	}
 	@Override
 	public int 	make_bet(java.util.ArrayList<Hand> last_table, int total_player, int my_position){
-		return 0;
+		return 1;
 	}
 	@Override
 	public java.lang.String toString(){
